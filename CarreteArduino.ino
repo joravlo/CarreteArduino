5e5de75dec54505e79f6d7a27ecc65b85a551778
Jordi Aviño <joravlo@gmail.com>
2021-01-20 21:43:22 +0100

    Añadido giro motor manual

--- CarreteArduino.ino
+++ CarreteArduino.ino
@@ -33,6 +33,23 @@ unsigned int localPort = 2000;      // local port to listen on
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; //buffer to hold incoming packet,
 char  ReplyBuffer[] = "acknowledged\r\n";       // a string to send back
 int lastDepth = 0;
+int countDepth = 0;
+int countOk = 0;
+float countCable = 0;
+boolean isManual = false;
+const long interval = 1000; 
+unsigned long previousMillis = 0; 
+
+//Pines
+const int pinButtonUp = D6;
+const int pinButtonDown = D7;
+const int pinButtonOk = D8;
+const int pinCountCable = D5;
+const int pinMotorLeft = D3;
+const int pinMotorRight = D4;
+const int pinMotorPWM = D2;
+const int pinModo = D0;
+const int pinDirManual = D9;
 
 WiFiUDP Udp;
 
@@ -40,10 +57,22 @@ int getDepth(String sentence); // Función que devuelve la profundida
 String getValue(String data, char separator, int index);
 void printLcd(String message);
 void printMeters(String sentence);
+void printTable(String meters, String count, float countCable);
+void rotateLeft();
+void rotateRight();
+void stopMotor();
 
 
 void setup() {
   Serial.begin(4800);
+
+  pinMode(pinButtonUp, INPUT);
+  pinMode(pinButtonDown, INPUT);
+  pinMode(pinButtonOk, INPUT);
+  pinMode(pinMotorLeft, OUTPUT);
+  pinMode(pinMotorRight, OUTPUT);
+  pinMode(pinModo, INPUT);
+  pinMode(pinDirManual, INPUT);
   
   Wire.begin(4, 5);
   lcd.init();
@@ -65,10 +94,17 @@ void setup() {
 }
 
 void loop() {
+  int stateButtonUp = digitalRead(pinButtonUp);
+  int stateButtonDown = digitalRead(pinButtonDown);
+  int stateButtonOk = digitalRead(pinButtonOk);
+  int stateCountCable = digitalRead(pinCountCable);
+
   // if there's data available, read a packet
+  unsigned long currentMillis = millis();
   int packetSize = Udp.parsePacket();
-  if (packetSize) {
+  if (packetSize && currentMillis - previousMillis >= interval) {
     // read the packet into packetBufffer
+    previousMillis = currentMillis;
     int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
     packetBuffer[n] = 0;
     Serial.println("Contents:");
@@ -82,6 +118,51 @@ void loop() {
     Udp.endPacket();
   }
 
+  if (pinModo == LOW) {
+    isManual = true;
+  } else {
+    isManual = false;  
+  }
+
+  if(stateButtonUp == 1) {
+    countDepth++;
+    String count = String(countDepth);
+    String lastDepthString = String(lastDepth);
+    printTable(lastDepthString, count, countCable);
+    delay(500);
+  }
+
+  if(stateButtonDown == 1) {
+    countDepth--;
+    String count = String(countDepth);
+    String lastDepthString = String(lastDepth);
+    printTable(lastDepthString, count, countCable);
+    delay(500);
+  }
+
+  if(stateButtonOk == 1){
+      countOk = countDepth;
+      delay(500);
+    }
+
+ if (stateCountCable == LOW) {
+    if (!isManual && pinDirManuel == HIGH) {
+        countCable = countCable + 0.25;
+        String count = String(countDepth);
+        String lastDepthString = String(lastDepth);
+        printTable(lastDepthString, count, countCable);
+     } else if (!isManual && pinDirManuel == LOW) {
+      countCable = countCable - 0.25;
+        String count = String(countDepth);
+        String lastDepthString = String(lastDepth);
+        printTable(lastDepthString, count, countCable);  
+      }
+
+     while(stateCountCable == LOW){                 //antirebote
+        stateCountCable = digitalRead(pinCountCable);
+      }
+  }   
+
 }
 
 void printLcd(String message){
@@ -93,10 +174,27 @@ void printLcd(String message){
 void printMeters(String sentence){
   if (sentence.substring(0,6) == "$INDPT"){
     String depth = getValue(sentence, ',', 1);
-    String message = "Metros: " + depth;
     lastDepth = getDepth(sentence);
-    printLcd(message);
+    String count = String(countDepth);
+    printTable(depth, count, countCable);
    }
+}
+
+void printTable(String meters, String count, float countCable) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Sonda");
+  lcd.setCursor(1, 1);
+  lcd.print(meters);
+  lcd.setCursor(6, 0);
+  lcd.print("Fondo");
+  lcd.setCursor(8, 1);
+  lcd.print(count);
+  lcd.setCursor(12, 0);
+  lcd.print("Cable");
+  lcd.setCursor(12, 1);
+  String countCableString = String(countCable);
+  lcd.print(countCableString);
 }
 
 int getDepth(String sentence) {
@@ -123,3 +221,19 @@ String getValue(String data, char separator, int index)
     }
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
+
+void rotateLeft(){
+  digitalWrite (pinMotorLeft, HIGH);
+  digitalWrite (pinMotorRight, LOW); 
+  }
+
+
+void rotateRight(){
+  digitalWrite (pinMotorRight, HIGH);
+  digitalWrite (pinMotorLeft, LOW); 
+  }
+
+void stopMotor() {
+  digitalWrite (pinMotorRight, LOW);
+  digitalWrite (pinMotorLeft, LOW);
+  }
